4aabe49b1dacbf65a489523e2c531eee6b974697
Youngki Park <ykp7409.gmail.com>
2024-07-30 19:24:57 +0900

    Modified codes
    Signed-off-by: Youngki Park <ykp7409.gmail.com>

--- tester670/tester670.ino
+++ tester670/tester670.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include "stdio.h"
+
 #define PRODUCT       "=-=-=-= Tester_670 =-=-=-="
 #define MAJOR_VERSION "0"
-#define MINOR_VERSION "1"
+#define MINOR_VERSION "2"
 #define VERSION       ("v" MAJOR_VERSION "." MINOR_VERSION)
-
 
 //#define DEBUG_DETAIL
 #define _NANO
@@ -14,8 +14,8 @@
 #define SERIAL_SPEED    115200
 
 // Global --------------
-int pin_WData[4] = { A1, A3, A2, A0 };
-int pin_RData[4] = { 6, 7, 8, 9 };
+int pin_WData[4] = { A1, A3, A2, A0 };  //D0 D1 D2 D3
+int pin_RData[4] = { 6, 7, 8, 9 };      //Q0 Q1 Q2 Q3
 
 int pin_WAddr[2] = { 2, 3 };
 int pin_RAddr[2] = { 10, 12 };
@@ -23,7 +23,7 @@ int pin_RAddr[2] = { 10, 12 };
 const int pin_WE = 4;
 const int pin_RE = 5;
 
-const int pin_POWER = 0;
+const int pin_POWER = A6;
 
 #ifdef _NANO
 void DBG(String msg)          { Serial.print(msg); }
@@ -159,49 +159,52 @@ byte Read670( byte addr )
 }
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
 #ifdef _NANO
-  Serial.begin(SERIAL_SPEED); 
-  Serial.println( String(PRODUCT) + String(" ") + String(VERSION) );
+    Serial.begin(SERIAL_SPEED); 
+    Serial.println( String(PRODUCT) + String(" ") + String(VERSION) );
 #else
-  SerialUSB.begin(SERIAL_SPEED); 
-  while (!SerialUSB) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  SerialUSB.println( String(PRODUCT) + String(" ") + String(VERSION) );
+    SerialUSB.begin(SERIAL_SPEED); 
+    while (!SerialUSB) {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+    SerialUSB.println( String(PRODUCT) + String(" ") + String(VERSION) );
 #endif
 
-  pinMode( pin_WE, OUTPUT );
-  pinMode( pin_WAddr[0], OUTPUT );
-  pinMode( pin_WAddr[1], OUTPUT );
+    pinMode( pin_WE, OUTPUT );
+    pinMode( pin_WAddr[0], OUTPUT );
+    pinMode( pin_WAddr[1], OUTPUT );
 
-  pinMode( pin_RE, OUTPUT );
-  pinMode( pin_RAddr[0], OUTPUT );
-  pinMode( pin_RAddr[1], OUTPUT );
-    
-  for( int i=0; i<4; i++ )
-  {
-      pinMode( pin_WData[i], OUTPUT );
-      pinMode( pin_RData[i], INPUT_PULLUP );
-  }
+    pinMode( pin_RE, OUTPUT );
+    pinMode( pin_RAddr[0], OUTPUT );
+    pinMode( pin_RAddr[1], OUTPUT );
 
-  digitalWrite( pin_WE, HIGH );
-  digitalWrite( pin_RE, HIGH );
+    pinMode( pin_POWER, OUTPUT );
 
+    for( int i=0; i<4; i++ ){
+        pinMode( pin_WData[i], OUTPUT );
+        pinMode( pin_RData[i], INPUT_PULLUP );
+    }
+
+    digitalWrite( pin_WE, HIGH );
+    digitalWrite( pin_RE, HIGH );
+    digitalWrite( pin_POWER, LOW );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
     DBGLN("PHASE 0");
-    for (int a=0; a<4; a++){
-      byte data;
-      data = Read670(a);
-      DBG( "add : " );
-      DBG( a );
-      DBG( " data : " );
-      DBGHEXLN( data );
+    for(int a=0; a<4; a++)
+    {
+        byte data;
+        data = Read670(a);
+        DBG( "add : " );
+        DBG( a );
+        DBG( " data : " );
+        DBGHEXLN( data );
     }
     DBGPAUSE();
+
 #if 0
     DBGLN("PHASE 1");
     // Store and check 2 different values in 4 addresses one address at a time. Total 8 checks for phase 1
